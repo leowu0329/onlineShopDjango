f6ebee5973f5e6cba728e461e0af89eaa6c21037
leowu0329 <ryowuandjanet@gmail.com>
2025-07-08 01:16:48 +0800

    add orders

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
     'shop.apps.ShopConfig',
 ]
 
